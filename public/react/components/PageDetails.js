880332108049e0c083d0b67be74cc4b81335aa98
Calvin La <calvin.la@clover.com>
2024-11-25 15:16:17 -0800

    Fixed issue with key prop in PageDetails.js

--- public/react/components/PageDetails.js
+++ public/react/components/PageDetails.js
@@ -8,7 +8,7 @@ export const PageDetails = ({ page, goBack }) => {
       <h1>{page.title}</h1>
       <p><strong>Author:</strong> {page.author.name}</p>
       <p><strong>Content:</strong> {page.content}</p>
-      <p><strong>Tags:</strong> {page.tags.map((tag,id) => {return <p>{tag.name}</p>} ) } </p>
+      <p><strong>Tags:</strong> {page.tags.map((tag,id) => {return <span key = {id}>{tag.name}</span>} ) } </p>
       <p><strong>Created At:</strong> {new Date(page.createdAt).toLocaleString()}</p>
     </div>
   );
